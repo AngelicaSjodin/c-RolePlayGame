1890b41daecae3daf89c4619fd99a8a8d49f4a8c
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-11-04 13:31:01 +0100

    value and boolean

--- Item.cpp
+++ Item.cpp
@@ -1,10 +1,18 @@
 
 #include "Item.h"
+#include <iostream>
 
-Item::Item(const std::string &n)
-    : name(n){}
+Item::Item(const std::string &name, int value)
+    : name(name),value(value){}
 
 std::string Item::getName() const{
     return name;
 }
 
+int Item::getValue() const{
+    return value;
+}
+
+bool Item::isConsumable() const{
+    return false;
+}
