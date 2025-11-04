fcabbbbbdff17965a5accd95c90156360e19d823
josefinkl <josefin.klangefjall@gmail.com>
2025-11-04 10:00:06 +0100

    armor.h added damageProtector

---
+++ Armor.h
@@ -0,0 +1,17 @@
+#include <string>
+#ifndef C_ROLEPLAYGAME_WEAPON_H
+#define C_ROLEPLAYGAME_WEAPON_H  
+#include "Item.h"
+
+class Armor : public Item {
+
+    public:
+    Armor(const std::string &name, int damageProtector);
+
+    private:
+    
+    int damageProtector; //How much protection the armor gives.
+    
+
+};
+#endif // C_ROLEPLAYGAME_WEAPON_H
