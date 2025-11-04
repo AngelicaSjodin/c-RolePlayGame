461f4598dc55eb0a7c7d808155aeb1a58bbd754b
JosefinKl <josefin.klangefjall@gmail.com>
2025-11-04 13:01:33 +0100

    created armor (#4)
    * created armor
    
    * armor
    
    * armor.h added damageProtector
    
    * added in armor.cpp
    
    * changed in armor.cpp
    
    * changes

---
+++ Armor.cpp
@@ -0,0 +1,6 @@
+#include "Armor.h"
+
+Armor::Armor(const std::string& name, int damageProtection)
+    : Item(name), damageProtection(damageProtection){
+
+    }
---
+++ Armor.h
@@ -0,0 +1,17 @@
+#include <string>
+#ifndef C_ROLEPLAYGAME_Armor_H
+#define C_ROLEPLAYGAME_Armor_H  
+#include "Item.h"
+
+class Armor : public Item {
+
+    public:
+    Armor(const std::string& name, int damageProtection);
+
+    private:
+    
+    int damageProtection; //How much protection the armor gives.
+    
+
+};
+#endif // C_ROLEPLAYGAME_Armor_H
