34738f040d16b7cff5a799f794911034d58a188f
LindaMU24 <3tomu24.blomli@folkuniversitetet.nu>
2025-11-05 11:35:51 +0100

    Weapon linda (#14)
    * Create Weapon - Linda
    
    * Create Weapon-Linda
    
    * Removed unwanted folder
    
    * Adjust weapon - Linda
    
    * Add weaponType and bool - Linda
    
    * Add value - Linda

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,12 +1,19 @@
 #include "Weapon.h"
 
 Weapon::Weapon(const std::string &n, int dmg, WeaponType _type)
-    : Item(n, 0, ItemType::Weapon), damage(dmg) {type=_type;}
+    : Item(n, 40, ItemType::Weapon), damage(dmg) {type=_type;}
 
 int Weapon::getDamage() const {
     return damage;
 }
 
+WeaponType Weapon::getType() const {
+    return type;
+}
+
+bool Weapon::isConsumable() const {
+    return false; // Weapons are not consumable
+}
 WeaponType Weapon::getType() const {
     return type;
 }
